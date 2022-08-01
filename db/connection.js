06d065fff8e35ff24f8f246d3d533bab83e18156
alevans99 <alevans99@gmail.com>
2022-08-01 18:32:25 +0100

    Completes setup for hosting

--- db/connection.js
+++ db/connection.js
@@ -1,34 +1,34 @@
 /**
  * This sets up the database based on the current environment (test/production)
  */
-const { Pool } = require("pg");
+const { Pool } = require('pg')
 
 //Defaults to development it NODE_ENV not provided (will be set by Jest if testing)
-const ENV = process.env.NODE_ENV || "development";
+const ENV = process.env.NODE_ENV || 'development'
 
 //Point dotenv towards which environment variables we want to use
 //(This will update which DB we use when switching between testing and production)
-require("dotenv").config({
+require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
-});
+})
 
 //Check that the database (set in .env) and db name (set in scripts) have been set.
-if (!process.env.PGDATABASE && !process.env.PGDATABASE_URL) {
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error(
-    "Either the DB or the DB url has not been set in environment variables"
-  );
+    'Either the DB or the DB url has not been set in environment variables'
+  )
 }
 
 //Only create a config for the DB Pool if we are in production. (we won't have
 //a DB url if in test as we will use local DB)
 const config =
-  ENV === "production"
+  ENV === 'production'
     ? {
         connectionString: process.env.DATABASE_URL,
         ssl: {
-          rejectUnauthorised: false,
+          rejectUnauthorized: false,
         },
       }
-    : {};
+    : {}
 
-module.exports = new Pool(config);
+module.exports = new Pool(config)
--- db/development-data/notes.js
+++ db/development-data/notes.js
@@ -1,35 +1,35 @@
+const { DateTime } = require('luxon')
 module.exports = [
-    {
-      listId: "dev",
-      noteTitle: 'This is the title',
-      noteText: 'This is the text of the first example note',
-      timestamp: DateTime.fromMillis(1649001286757).toSQL(),
-      priority: 1,
-      deadline: DateTime.fromMillis(1650150000000).toSQL(),
-    },
-    {
-      listId: "dev",
-      noteTitle: 'Second Test Title',
-      noteText: 'This is the text of the second example note',
-      timestamp: DateTime.fromMillis(1629001286757).toSQL(),
-      priority: 2,
-      deadline: DateTime.fromMillis(1690150000000).toSQL(),
-    },
-    {
-      listId: "another dev",
-      noteTitle: 'First Other Test Title',
-      noteText: 'This is the text for the first note for the other list',
-      timestamp: DateTime.fromMillis(1629001286757).toSQL(),
-      priority: 2,
-      deadline: DateTime.fromMillis(1690150000000).toSQL(),
-    },
-    {
-      listId: "another dev",
-      noteTitle: 'Second Other Test Title',
-      noteText: 'This is the text for the second note for the other list',
-      timestamp: DateTime.fromMillis(1619001286757).toSQL(),
-      priority: 2,
-      deadline: DateTime.fromMillis(1790150000000).toSQL(),
-    },
-  ];
-  
+  {
+    listId: 'dev',
+    noteTitle: 'This is the title',
+    noteText: 'This is the text of the first example note',
+    timestamp: DateTime.fromMillis(1649001286757).toSQL(),
+    priority: 1,
+    deadline: DateTime.fromMillis(1650150000000).toSQL(),
+  },
+  {
+    listId: 'dev',
+    noteTitle: 'Second Test Title',
+    noteText: 'This is the text of the second example note',
+    timestamp: DateTime.fromMillis(1629001286757).toSQL(),
+    priority: 2,
+    deadline: DateTime.fromMillis(1690150000000).toSQL(),
+  },
+  {
+    listId: 'another dev',
+    noteTitle: 'First Other Test Title',
+    noteText: 'This is the text for the first note for the other list',
+    timestamp: DateTime.fromMillis(1629001286757).toSQL(),
+    priority: 2,
+    deadline: DateTime.fromMillis(1690150000000).toSQL(),
+  },
+  {
+    listId: 'another dev',
+    noteTitle: 'Second Other Test Title',
+    noteText: 'This is the text for the second note for the other list',
+    timestamp: DateTime.fromMillis(1619001286757).toSQL(),
+    priority: 2,
+    deadline: DateTime.fromMillis(1790150000000).toSQL(),
+  },
+]
