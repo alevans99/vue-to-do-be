06691d3770d0b7eed1c7f62ac124f91247bb07eb
alevans99 <alevans99@gmail.com>
2022-11-22 13:20:59 +0000

    Updates backend deployment

--- db/connection.js
+++ db/connection.js
@@ -29,6 +29,11 @@ const config =
           rejectUnauthorized: false,
         },
       }
-    : {}
+    : {
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT
+    }
 
 module.exports = new Pool(config)
